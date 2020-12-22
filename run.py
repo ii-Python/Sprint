699d9e635236182ebc581e844f2ae25d66a6e6f2
iiPython <ben@iipython.cf>
2020-12-21 20:28:51 -0600

    Label python version and host system

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 # Modules
 import sprint
+import platform
 
 # Credit message
 print(sprint.colored(f"Sprint v{sprint.__version__} by iiPython", "yellow"))
+print(sprint.colored(f"Python version {platform.python_version()}, running on {platform.system()}", "yellow"))
 print()
 
 # Main loop
 while True:
 
     try:
-        cmd = input(sprint.colored(">>> ", "green"))
+        cmd = input(sprint.colored("Sprint >>> ", "green"))
 
     except KeyboardInterrupt:
         print()  # Stop weird line break issues
