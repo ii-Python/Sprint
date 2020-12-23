418f39752cef739ac5daa0edef2db4e0bcd77286
iiPython <ben@iipython.cf>
2020-12-22 23:00:02 -0600

    New multi-line support

--- run.py
+++ run.py
@@ -8,17 +8,32 @@ print(sprint.colored(f"Sprint v{sprint.__version__} by iiPython", "yellow"))
 print(sprint.colored(f"Python version {platform.python_version()}, running on {platform.system()}", "yellow"))
 print()
 
-# Main loop
-parser = sprint.SprintParser()
-while True:
+# Command grabber
+def get_command(indent = 0):
 
     # Set our path
     path = os.getcwd()
     path = path.replace(os.getenv("HOME"), "~")
 
+    # Fetch our command
+    command = input(sprint.colored(f"{path} >>> {' ' * indent}", "green"))
+
+    # Multi-line support
+    if command.endswith("\\"):
+        command = command[:-1]
+
+        command = command + get_command(indent = indent + 2)
+
+    # Return
+    return command
+
+# Main loop
+parser = sprint.SprintParser()
+while True:
+
     # Execute command
     try:
-        cmd = input(sprint.colored(f"Sprint@{path} >>> ", "green"))
+        cmd = get_command()
 
     except KeyboardInterrupt:
         print()  # Stop weird line break issues
